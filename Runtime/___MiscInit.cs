4dba2756b3146412eb1738d1b9525a3643862524
覃健培 <3357709076@qq.com>
2026-06-06 21:54:25 +0800

    1

--- Runtime/___MiscInit.cs
+++ Runtime/___MiscInit.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace Lookloop.ItemManager
 {
 
 public static class MiscInit
 {
+    public static Image itemImage;
+    public static Image edge;
+    public static TextMeshProUGUI count;
 
+    /// <summary>在 parent 下生成 ItemUI 三兄弟空对象，索引写入静态字段</summary>
+    public static void CreateItemUI(Transform parent)
+    {
+        var itemUIGo = new GameObject("ItemUI", typeof(RectTransform), typeof(Image));
+        itemUIGo.transform.SetParent(parent, false);
+        itemImage = itemUIGo.GetComponent<Image>();
+
+        var edgeGo = new GameObject("edge", typeof(RectTransform), typeof(Image));
+        edgeGo.transform.SetParent(parent, false);
+        edge = edgeGo.GetComponent<Image>();
+
+        var countGo = new GameObject("count", typeof(RectTransform), typeof(TextMeshProUGUI));
+        countGo.transform.SetParent(parent, false);
+        count = countGo.GetComponent<TextMeshProUGUI>();
+    }
 }
 
 }
