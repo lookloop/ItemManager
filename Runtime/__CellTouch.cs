be8cc25fdd81f3390badb5e87a6782f4a793c4ad
覃健培 <3357709076@qq.com>
2026-06-06 21:48:21 +0800

    1

--- Runtime/__CellTouch.cs
+++ Runtime/__CellTouch.cs
@@ -10,7 +10,7 @@ public static class CellTouch
     {
         public static void LongPress(Core core)
         {
-            Debug.Log($"[CellTouch] 长按触发 — tag:{core.atTag}  holdTime:{core.holdTime:F2}");
+            
         }
     }
 }
