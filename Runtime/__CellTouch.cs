689d6adfb0ca0acdd66a96017ec2136bea3cafdc
覃健培 <3357709076@qq.com>
2026-06-06 22:24:13 +0800

    1

--- Runtime/__CellTouch.cs
+++ Runtime/__CellTouch.cs
@@ -9,10 +9,12 @@ namespace Lookloop.ItemManager
 public static class CellTouch
     {
         public static bool isLongPress;
+        public static PointerEventData downEvent;
 
         public static void BeginTouch(Core core, PointerEventData eventData)
         {
             isLongPress = false;
+            downEvent = eventData;
             Debug.Log($"[CellTouch] 按下 — cell:{core.hitRect?.name}");
         }
 
