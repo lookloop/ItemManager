704d83ae8d8cf94a103fb12f87bc5acc5e7edd9f
覃健培 <3357709076@qq.com>
2026-06-06 22:06:13 +0800

    1

--- Runtime/Core_Init.cs
+++ Runtime/Core_Init.cs
@@ -15,6 +15,8 @@ public partial class Core
         ContainerBuilder.BuildAll(this);
         TestItemFiller.FillAll(this, 20);
         InvokeRepeating(nameof(ReleaseStaleHandles), 1800f, 1800f); // 每 30 分钟清理过期句柄
+        MiscInit.CreateTemporaryItemUI();
+
     }
 
 }
