6bd417b664559af03705e2dff0c15f155e94e6cf
覃健培 <3357709076@qq.com>
2026-06-08 22:15:21 +0800

    1

---
+++ Runtime/____ItemView.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Lookloop.ItemManager
+{
+    /// <summary>
+    /// 物品视图 — 负责 Cell 内物品的显示/刷新逻辑。
+    /// </summary>
+    public static class ItemView
+    {
+        
+    }
+}
