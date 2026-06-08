f5ac969b03a5b46b993ec6e75c389b02efe3608d
覃健培 <3357709076@qq.com>
2026-06-09 02:56:55 +0800

    save

--- Runtime/__TouchContainer.cs
+++ Runtime/__TouchContainer.cs
@@ -32,7 +32,30 @@ namespace Lookloop.ItemManager
             Vector2 diff = OnDragPos - core.onPos;
             //新的位置设置，原来的位置+距离。将差异弥补，差异是手指移动造成的，也就是有多少差异就移动多少，跟随手指移动。
             core.sourceRect.anchoredPosition = core.sourcePos + diff;
-        }
 
+            //射线检测当前拖拽下方碰到的对象
+            var raycast = core.eventData.pointerCurrentRaycast;
+            if (raycast.gameObject != null)
+            {
+                core.targetRect = raycast.gameObject.GetComponent<RectTransform>();
+                // 沿父级向上找 tag=Container
+                var t = core.targetRect != null ? core.targetRect.parent : null;
+                while (t != null)
+                {
+                    if (t.CompareTag("Container"))
+                    {
+                        if (int.TryParse(t.name, out int index) && index < core.containers.Length)
+                            core.targetContainer = core.containers[index];
+                        break;
+                    }
+                    t = t.parent;
+                }
+            }
+            else
+            {
+                core.targetRect = null;
+                core.targetContainer = null;
+            }
+        }
     }
 }
