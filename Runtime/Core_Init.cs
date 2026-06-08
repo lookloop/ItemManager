b2d5c969e660a99e30422ea0708dddc17140e5dd
覃健培 <3357709076@qq.com>
2026-06-08 20:32:44 +0800

    1

--- Runtime/Core_Init.cs
+++ Runtime/Core_Init.cs
@@ -7,25 +7,29 @@ public partial class Core
 {
     void Awake()
     {
+        //frame设置为60
         Application.targetFrameRate = 60;
+        //获取一下canvas是ui工作当中很重要的事情
         canvas = GetComponentInParent<Canvas>();
     }
 
     void Start()
     {
+        //启动构建功工作
         ContainerBuilder.BuildAll(this);
+        //开启磁盘资源本地缓存循环检查，去除时间留得太久的
         StartCoroutine(LossTimeLoop());
 
     }
     IEnumerator LossTimeLoop()
-{
-    yield return new WaitForSeconds(1800f);  // 首次等 30 分钟
-    while (true)
     {
-        LossTime();
-        yield return new WaitForSeconds(1800f);  // 每 30 分钟一次
+        yield return new WaitForSeconds(1800f);  // 首次等 30 分钟
+        while (true)
+        {
+            LossTime();
+            yield return new WaitForSeconds(1800f);  // 每 30 分钟一次
+        }
     }
-}
 
 }
 }
