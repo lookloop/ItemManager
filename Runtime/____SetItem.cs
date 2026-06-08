db73cfb459cc88c7e9b43f08480c75a96cc12c1a
覃健培 <3357709076@qq.com>
2026-06-08 22:17:27 +0800

    1

--- Runtime/____SetItem.cs
+++ Runtime/____SetItem.cs
@@ -5,7 +5,7 @@ namespace Lookloop.ItemManager
     /// <summary>
     /// 物品视图 — 负责 Cell 内物品的显示/刷新逻辑。
     /// </summary>
-    public static class ItemView
+    public static class SetItem
     {
         
     }
