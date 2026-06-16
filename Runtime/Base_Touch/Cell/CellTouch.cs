4cac6fd61ce34a598aaf09a4c73b31b23388f84d
覃健培 <3357709076@qq.com>
2026-06-16 09:44:06 +0800

    save

--- Runtime/Base_Touch/Cell/CellTouch.cs
+++ Runtime/Base_Touch/Cell/CellTouch.cs
@@ -74,5 +74,43 @@ namespace Lookloop.ItemManager
                 longPressCoroutine = null;
             }
         }
+
+        void Exchange()
+        {
+            if (targetCell == null) return;
+
+            int srcKey = container.cells.Length * (container.currentPage - 1) + cellKey;
+            int tgtKey = targetCell.container.cells.Length * (targetCell.container.currentPage - 1) + targetCell.cellKey;
+
+            core.Exchange(container, srcKey, targetCell.container, tgtKey);
+        }
+
+        void Reset()
+        {
+            if (container != null && container.cells != null &&
+                core.sourceContainer == container)
+            {
+                int start = container.cells.Length * (container.currentPage - 1);
+                int end = UnityEngine.Mathf.Min(
+                    container.cells.Length * container.currentPage - 1,
+                    container.items.Length - 1);
+                if (core.sourceItemKey >= start && core.sourceItemKey <= end)
+                    core.Launch(
+                        core.View(container, core.sourceItemKey));
+            }
+
+            targetCell = null;
+            lastTurnTime = 0f;
+            isDrag = false;
+            isLongPress = false;
+
+            core.sourceContainer = null;
+            core.sourceItemKey = 0;
+
+            core.dragParent.gameObject.SetActive(false);
+            core.Shadow.gameObject.SetActive(false);
+
+            CancelLongPress();
+        }
     }
 }
